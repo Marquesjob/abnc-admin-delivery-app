3f86c8b91e2fb50c0059d5942bc555f707b0f5ee
Sagaz <patrickmarques.job@gmail.com>
2023-06-17 01:07:27 -0300

    feat: [ #fundation ]
    Desenvolvido o componente de menu lateral

---
+++ src/app/structures/fundation/fundation-content.settings.ts
@@ -0,0 +1,8 @@
+export const fundationContentSettings = {
+  sideMenu: {
+    list: [
+      { name: 'Dashboard', routerLink: '#', icon: '#' },
+      { name: 'Gerar Pedido', routerLink: '#', icon: '#' }
+    ]
+  }
+}
