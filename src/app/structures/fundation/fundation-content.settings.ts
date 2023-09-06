724f8da59701e4fd80e7d1eb5328bb32581bde74
Sagaz <patrickmarques.job@gmail.com>
2023-09-05 23:56:48 -0300

    WIP

--- src/app/structures/fundation/fundation-content.settings.ts
+++ src/app/structures/fundation/fundation-content.settings.ts
@@ -2,7 +2,8 @@ export const fundationContentSettings = {
   sideMenu: {
     list: [
       { name: 'Dashboard', routerLink: '#', icon: '#' },
-      { name: 'Gerar Pedido', routerLink: '#', icon: '#' }
+      { name: 'Gerar Pedido', routerLink: '#', icon: '#' },
+      { name: '//Aumentar largura', routerLink: '#', icon: '#' }
     ]
   }
 }
