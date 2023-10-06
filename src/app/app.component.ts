3f44c9d91374c59e3315c1a8fce178aa8def7f4c
Sagaz <patrickmarques.job@gmail.com>
2023-10-06 01:57:12 -0300

    WIP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { ChangeDetectorRef, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Sabor e Empatia';
+
+  public contentSettings: any = {
+  list: [
+    { name: 'Dashboard', routerLink: '#', icon: '#' },
+    { name: 'Emitir Comanda', routerLink: '#', icon: '#' },
+    { name: 'Gerar Pedido', routerLink: '#', icon: '#' },
+  ]
+  };
+
+  public menuContents: any = this.contentSettings.list;
+  public mobileQuery: MediaQueryList;
+  private _mobileQueryListener: () => void;
+
+  //API responses
+  public demands!: Array<any>;
+
+  constructor(
+    changeDetectorRef: ChangeDetectorRef, media: MediaMatcher,
+
+  ) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,34 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StructuresModule } from './structures/structures.module';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { NgIf, NgFor, CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    StructuresModule
+    MatSidenavModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    HttpClientModule,
+    NgFor,
+    NgIf,
+    StructuresModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
